db0e6a6b7aacacd1c558b36f9b1c592bfdd0643b
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-10-26 13:16:19 +0500

    Add class definition of TextEditor class

---
+++ include/TextEditor.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include "DoublyLinkedList.hpp"
+using namespace std;
+
+class TextEditor{
+    private:
+        DoublyLinkedList<DoublyLinkedList<char>> text;
+        Node<char>* currentNode;
+    public:
+        
+};
