0723293c407c72792293639d753796f9d838fe89
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-11-04 00:13:27 +0500

    Update Doubly Linked List

--- include/DoublyLinkedList.hpp
+++ include/DoublyLinkedList.hpp
@@ -19,11 +19,22 @@ class DoublyLinkedList{
         int size;
     public:
         DoublyLinkedList(){
+            head = nullptr;
+            tail = nullptr;
+        }
 
+        DoublyLinkedList(Node<T>* a){
+            head = a;
+            tail = head;
         }
 
         ~DoublyLinkedList(){
-
+            Node<T>* cptr = head;
+            while (cptr != nullptr) {
+                Node<T>* temp = cptr;
+                cptr = cptr->next;
+                delete temp;
+            }
         }
 
         void insertAtHead(T value){
@@ -54,15 +65,27 @@ class DoublyLinkedList{
             
         }
 
+        // This is the setpos function. Made for more efficiency
         Node<T>* operator[](int index){
+            if (index < 0) throw out_of_range("Negative index");
+            Node* ptr = head;
+            int temp = 0;
+            while(temp != index){
+                temp++;
+                ptr = ptr->next;
+            }
 
+            if (ptr == nullptr)
+                throw std::out_of_range("Index out of range");
+
+            return ptr;
         }
 
         bool isEmpty(){
-
+            return size == 0;
         }
 
         int getSize(){
-
+            return size;
         }
 };
