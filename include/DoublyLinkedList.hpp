b0b3bfeb1931f677a01508e96290aa06c1b1ee98
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-11-04 12:25:04 +0500

    Add completed implementation of doubly linked list

--- include/DoublyLinkedList.hpp
+++ include/DoublyLinkedList.hpp
@@ -23,11 +23,6 @@ class DoublyLinkedList{
             tail = nullptr;
         }
 
-        DoublyLinkedList(Node<T>* a){
-            head = a;
-            tail = head;
-        }
-
         ~DoublyLinkedList(){
             Node<T>* cptr = head;
             while (cptr != nullptr) {
@@ -38,27 +33,112 @@ class DoublyLinkedList{
         }
 
         void insertAtHead(T value){
-            
+            Node<T>* newNode = new Node<T>(value);
+            size++;
+            if(head == nullptr){
+                head = newNode;
+                tail = newNode;
+                return;
+            }
+
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
         }
 
         void insertAtTail(T value){
+            Node<T>* newNode = new Node<T>(value);
+            size++;
+            if(tail == nullptr){
+                head = newNode;
+                tail = newNode;
+                return;
+            }
 
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
         }
 
-        void insertAtIndex(T value){
+        void insertAtIndex(int index, T value){
+            if(index < 0) throw out_of_range("Negative index");
+            if(index > size) throw out_of_range("Index out of range");
 
+            if(index == 0){
+                insertAtHead(value);
+                return;
+            }
+            if(index == size){
+                insertAtTail(value);
+                return;
+            }
+
+            Node<T>* cur = head;
+            for(int i = 0; i < index - 1; i++){
+                cur = cur->next;
+            }
+
+            Node<T>* newNode = new Node<T>(value);
+            newNode->next = cur->next;
+            newNode->prev = cur;
+            current->next->prev = newNode;
+            current->next = newNode;
+            size++;
         }
 
         void removeFromHead(){
-
+            if(head == nullptr) throw out_of_range("List is empty");
+            
+            Node<T>* temp = head;
+            head = head->next;
+            if(head == nullptr){
+                tail = nullptr
+            }
+            else{
+                head->prev = nullptr;
+            }
+            delete temp;
+            size--;
         }
 
         void removeFromTail(){
-
+            if(tail == nullptr) throw out_of_range("List is empty");
+            
+            Node<T>* temp = tail;
+            tail = tail->prev;
+            if(tail != nullptr){
+                tail->next = nullptr;
+            }
+            else{
+                head = nullptr;
+            }
+            delete temp;
+            size--;
         }
 
-        void removeFromIndex(){
-
+        void removeFromIndex(int index){
+            if(index < 0) throw out_of_range("Negative index");
+            if(index >= size) throw out_of_range("Index out of range");
+            
+            if(index == 0){
+                removeFromHead();
+                return;
+            }
+            
+            if(index == size - 1){
+                removeFromTail();
+                return;
+            }
+            
+            Node<T>* current = head;
+            for(int i = 0; i < index; i++){
+                current = current->next;
+            }
+            
+            current->prev->next = current->next;
+            current->next->prev = current->prev;
+            delete current;
+            size--;
         }
 
         DoublyLinkedList<T> splitList(int index){
@@ -67,15 +147,14 @@ class DoublyLinkedList{
 
         Node<T>* operator[](int index){
             if (index < 0) throw out_of_range("Negative index");
+            if(index > size) throw out_of_range("Index out of range");
+
             Node* ptr = head;
             int temp = 0;
             while(temp != index){
                 temp++;
                 ptr = ptr->next;
             }
-
-            if (ptr == nullptr)
-                throw std::out_of_range("Index out of range");
 
             return ptr;
         }
--- include/Stack.hpp
+++ include/Stack.hpp
@@ -7,35 +7,56 @@ struct Node{
     T value;
     Node* next;
 
-    Node(T value, Node* next = nullptr) : value(value), next(next), prev(prev) {}
+    Node(T value, Node* next = nullptr) : value(value), next(next) {}
 };
 
 template <typename T>
 class Stack {
-private:
-    Node<T>* top;
-    int size;
-public:
-    Stack(){
+    private:
+        Node<T>* top;
+        int size;
+    public:
+        Stack(){
+            top = nullptr;
+            size = 0;
+        }
 
-    }
+        ~Stack(){
+            Node<T>* cur = top;
+            while(cur != nullptr){
+                Node<T>* temp = cur;
+                cur = cur->next;
+                delete temp;
+            }
+        }
 
-    ~Stack(){
+        void push(T value){
+            Node<T>* newNode = new Node<T>(value, top);
+            top = newNode;
+            size++;
+        }
 
-    }
+        T pop(){
+            if(isEmpty()){
+                throw out_of_range("Stack is empty");
+            }
+            
+            Node<T>* temp = top;
+            T value = top->value;
+            top = top->next;
+            delete temp;
+            size--;
+            
+            return value;
+        }
 
-    void push(T value){
+        T peek(){
+            if(isEmpty()){
+                throw out_of_range("Stack is empty");
+            }
+            return top->value;
+        }
 
-    }
-
-    T pop(){
-
-    }
-
-    T peek(){
-
-    }
-
-    bool isEmpty(){return topNode == nullptr;}
-    int getSize(){return size;}
+        bool isEmpty(){return top == nullptr;}
+        int getSize(){return size;}
 };
