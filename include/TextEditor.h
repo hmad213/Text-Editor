419f8e636bf453b9aa0f5c28b8cee67f20858e60
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-11-11 00:13:45 +0500

    Reorganize code

--- include/TextEditor.h
+++ include/TextEditor.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <iostream>
 #include "DoublyLinkedList.hpp"
 using namespace std;
--- src/TextEditor.cpp
+++ src/TextEditor.cpp
@@ -6,11 +6,11 @@ TextEditor::TextEditor(){
     currentNode = nullptr;
     DoublyLinkedList<char>* newList = new DoublyLinkedList<char>;
     text.insertAtHead(newList);
-    currentLineNode = text[0];
+    currentLineNode = text.getHead();
 }
 
 TextEditor::~TextEditor(){
-    Node<DoublyLinkedList<char>*>* currentLine = text[0];
+    Node<DoublyLinkedList<char>*>* currentLine = text.getHead();
     
     while (currentLine != nullptr) {
         DoublyLinkedList<char>* line = currentLine->value;
@@ -22,46 +22,49 @@ TextEditor::~TextEditor(){
 }
 
 void TextEditor::insertChar(char value){
+    // uses insertAtNode in DoublyLinkedList
     currentNode = currentLineNode->value->insertAtNode(currentNode, value);
     nodeIndex++;
-    cout << lineIndex << ", " << nodeIndex << endl;
 }
 
-// TODO: loop through the string and use insertChar to insert all the characters.
-// Specical case: \n. Use addNewLine()
 void TextEditor::insertString(string value){
 
 }
 
 void TextEditor::removeChar(){
+    // Special case: if cursor is at the start of line, remove the line
     if(!currentNode){
         removeLine();
         return;
     }
+
+    // uses removeFromNode in DoublyLinkedList
     currentNode = currentLineNode->value->removeFromNode(currentNode);
     nodeIndex--;
-    cout << lineIndex << ", " << nodeIndex << endl;
 }
 
 void TextEditor::removeCharFront(){
+    // TODO: add removing of line
+    // Special case: if cursor is at the start of line, remove from head if it exists
     if(!currentNode){
         if(!currentLineNode->value->getHead()) return;
         currentLineNode->value->removeFromHead();
         return;                    
     }
-    if(!currentNode->next) return;
+    if(!currentNode->next) return;  // TODO: add removing of line
+
+    // uses removeFromNode in DoublyLinkedList
     currentNode = currentLineNode->value->removeFromNode(currentNode->next);
 }
 
-// TODO: add a new line. A line in this case is a new node of DoublyLinkedList<char>*. Make sure to dynamically allocate
-// Special Case: Pressing enter in the middle of the line. use the function in DoublyLinkedList.hpp splitList
-// Note: The splitList function is not completed.
 void TextEditor::addNewLine(){
+    // if cursor is at the start of line, create a new line and insert correctly
     if(nodeIndex <= 0){
         DoublyLinkedList<char>* newList = new DoublyLinkedList<char>;
         text.insertAtIndex(lineIndex, newList);
         lineIndex++;
     }else{
+        // splitting of list into two and creating a new line when in middle of the string
         DoublyLinkedList<char>* newList = currentLineNode->value->splitList(nodeIndex - 1);
         text.insertAtNode(currentLineNode, newList);
         currentLineNode = currentLineNode->next;
@@ -71,11 +74,11 @@ void TextEditor::addNewLine(){
     }
 }
 
-// TODO: remove a line. A line in this case is a new node of DoublyLinkedList<char>*.
-// Special Case: Pressing backspace at the start of the line in which case it will merge this linked list with the previous one
-// Note: The function has to be implemented in DoublyLinkedList.hpp
 void TextEditor::removeLine(){
+    // condition to check if there are no lines before 
     if(lineIndex <= 0) return;
+
+    // merging of current line with previous one to form a singular line
 
     Node<DoublyLinkedList<char>*>* prevLine = currentLineNode->prev;
 
@@ -90,19 +93,12 @@ void TextEditor::removeLine(){
     currentLineNode = prevLine;
 }
 
-// TODO: adjusts the line depending on user input.
-// Changing X: update currentNode and nodeIndex. 
-// Special case for X: Moving x after end of line or before start of line
-// Changing Y: update currentLineNode, lineIndex, and currentNode while also adjusting to nodeIndex. 
-// Special case for Y: if nodeIndex exceeds possible nodeIndex in the new currentLineNode, then nodeIndex is at tail.
 void TextEditor::moveCursor(int x, int y){
-        cout << "Moving cursor: " << x << ", " << y << endl;
-    
-    // Handle vertical movement first
+    // for vertical
     if (y != 0) {
         int newLineIndex = lineIndex + y;
         
-        // Boundary check for vertical movement
+        // Boundary check
         if (newLineIndex < 0) newLineIndex = 0;
         if (newLineIndex >= text.getSize()) newLineIndex = text.getSize() - 1;
         
@@ -110,117 +106,103 @@ void TextEditor::moveCursor(int x, int y){
             lineIndex = newLineIndex;
             currentLineNode = text[lineIndex];
             
-            // Adjust horizontal position for the new line
-            DoublyLinkedList<char>* newLine = currentLineNode->value;
-            int newLineLength = newLine->getSize();
-            
-            if (nodeIndex > newLineLength) {
-                // If current horizontal position is beyond new line length, go to end
-                nodeIndex = newLineLength;
-                currentNode = newLine->getTail();
-            } else {
-                // Try to maintain similar horizontal position
-                if (nodeIndex == 0) {
-                    currentNode = nullptr; // At start of line
-                } else if (nodeIndex == newLineLength) {
-                    currentNode = newLine->getTail(); // At end of line
-                } else {
-                    currentNode = (*newLine)[nodeIndex];
-                }
+            // adjust horizontal position for the new line
+            if(nodeIndex == 0)
+                currentNode = nullptr;
+            else
+                currentNode = (*currentLineNode->value)[nodeIndex - 1]; // this gets the last element if node index > size
+
+            if(currentNode == currentLineNode->value->getTail()){
+                nodeIndex = currentLineNode->value->getSize();
             }
         }
     }
     
-    // Handle horizontal movement
+    // for horizontal
     if (x != 0) {
-        DoublyLinkedList<char>* currentLine = currentLineNode->value;
-        int lineLength = currentLine->getSize();
+        int lineLength = currentLineNode->value->getSize();
         
+        // for right
         if (x > 0) {
-            // Move right
             for (int i = 0; i < x; i++) {
                 if (nodeIndex < lineLength) {
-                    // Move right within current line
+                    // move right within current line
                     if (currentNode == nullptr) {
-                        // At start of line, move to first character
-                        currentNode = currentLine->getHead();
+                        // if at start of line, move to first character
+                        currentNode = currentLineNode->value->getHead();
                     } else {
                         currentNode = currentNode->next;
                     }
                     nodeIndex++;
                 } else if (currentLineNode->next != nullptr) {
-                    // Move to next line
+                    // move to next line
                     lineIndex++;
                     currentLineNode = currentLineNode->next;
-                    currentLine = currentLineNode->value;
-                    lineLength = currentLine->getSize();
-                    currentNode = nullptr; // Start of new line
+                    lineLength = currentLineNode->value->getSize();
+                    currentNode = nullptr; // start of new line
                     nodeIndex = 0;
                 }
-                // Else can't move further right
+                // else can't move further right
             }
-        } else {
-            // Move left
-            for (int i = 0; i < -x; i++) {
+        } else {    // for left
+
+            for (int i = 0; i < - x; i++) {
                 if (nodeIndex > 0) {
-                    // Move left within current line
-                    if (currentNode == nullptr) {
-                        // This shouldn't happen if nodeIndex > 0
-                        currentNode = currentLine->getTail();
-                    } else {
-                        currentNode = currentNode->prev;
-                    }
+                    // move left within current line
+                    currentNode = currentNode->prev;
                     nodeIndex--;
                 } else if (currentLineNode->prev != nullptr) {
-                    // Move to previous line
+                    // move to previous line
                     lineIndex--;
                     currentLineNode = currentLineNode->prev;
-                    currentLine = currentLineNode->value;
-                    lineLength = currentLine->getSize();
-                    currentNode = currentLine->getTail(); // End of previous line
+                    lineLength = currentLineNode->value->getSize();
+                    currentNode = currentLineNode->value->getTail(); // end of previous line
                     nodeIndex = lineLength;
                 }
-                // Else can't move further left
+                // else can't move further left
             }
         }
     }
     
-    cout << "New cursor position: " << lineIndex << ", " << nodeIndex << endl;
 }
 
 void TextEditor::setCursorPosition(int lineIndex, int nodeIndex){
+    // Vertical positioning
+    // boundary check
     if (lineIndex < 0) lineIndex = 0;
     if (lineIndex >= text.getSize()) lineIndex = text.getSize() - 1;
     
     currentLineNode = text[lineIndex];
     this->lineIndex = lineIndex;
     
-    DoublyLinkedList<char>* currentLine = currentLineNode->value;
-    if (nodeIndex > currentLine->getSize()) nodeIndex = currentLine->getSize();
+    //Horizontal positioning
+    if (nodeIndex > currentLineNode->value->getSize()) nodeIndex = currentLineNode->value->getSize();
     
     if (nodeIndex <= 0) 
         currentNode = nullptr;
     else
         currentNode = (*currentLineNode->value)[nodeIndex - 1];
     this->nodeIndex = nodeIndex;
-    
-    cout << this->lineIndex << ", " << this->nodeIndex << endl;
 }
 
 string TextEditor::getText(){
     string str = "";
     Node<DoublyLinkedList<char>*>* line = text[0];
+
+    // traverse and extract each char
     while(line != nullptr){
         Node<char>* cur = (*line->value)[0];
+
         while(cur != nullptr){
             str += cur->value;
             cur = cur->next;
         }
+
         line = line->next;
         if(line)
         str += '\n';
     }
-    cout << str << endl;
+
     return str;
 }
 
